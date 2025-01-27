380e7cf625e524bd4a5e5ba97880396b5b9c69e2
geektao1024 <471787092@qq.com>
2025-01-27 11:43:23 +0800

    更新导航栏

--- src/.vitepress/config/navbar.config.ts
+++ src/.vitepress/config/navbar.config.ts
@@ -4,6 +4,6 @@ export const nav: DefaultTheme.NavItem[] = [
   { text: '入门教程', link: '/wiki/user-guide/cursor-introduction', activeMatch: '/wiki/user-guide/' },
   { text: '文章博客', link: '/blog/' },
   { text: '官方文档', link: '/docs-zh/', activeMatch: '/docs-zh/' },
-  { text: '规则指南', link: '/rules/', activeMatch: '/rules/' },
+  { text: 'MUSIC AI', link: 'https://www.sunoaimusical.com', target: '_blank' },
   { text: 'ChatGPT 合租', link: 'https://nf.video/Up9Xa', target: '_blank' },
 ]
